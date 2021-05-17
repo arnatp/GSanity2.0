4a2e3d6f8a88d9c961c388e75a6f916032b99930
arnatp <arnautorresp@gmail.com>
2021-05-17 16:17:06 +0200

    Se elimina Rest Srvice por el uso de Firebase

--- REST_Service/src/main/java/cat/institutmarianao/service/ApplicationConfig.java
+++
@@ -1,9 +0,0 @@
-package cat.institutmarianao.service;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("rest")
-public class ApplicationConfig extends Application {
-
-}
--- REST_Service/src/main/webapp/index.jsp
+++
@@ -1,6 +0,0 @@
-<html>
-<body>
-<h2>Aplicación Restful desplegada</h2>
-<h4>Realizado por: Alberto, Arnau y Angel</h4>
-</body>
-</html>
