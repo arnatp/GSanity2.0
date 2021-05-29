95b2ad1672f2b496992a339784840ac621dedf7f
arnatp <arnautorresp@gmail.com>
2021-05-29 12:50:16 +0200

    se finalizan traducciones a falta de toast y mensajes de error

--- GSanityApp/src/app/pages/doctor/visits/visit/prescription/prescription.module.ts
+++ GSanityApp/src/app/pages/doctor/visits/visit/prescription/prescription.module.ts
@@ -7,14 +7,26 @@ import { IonicModule } from '@ionic/angular';
 import { PrescriptionPageRoutingModule } from './prescription-routing.module';
 
 import { PrescriptionPage } from './prescription.page';
-
+import { HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+export function createTranslateLoader(http: HttpClient) {
+	return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    PrescriptionPageRoutingModule
-  ],
-  declarations: [PrescriptionPage]
+	imports: [
+		CommonModule,
+		FormsModule,
+		IonicModule,
+		PrescriptionPageRoutingModule,
+		TranslateModule.forChild({
+			loader: {
+				provide: TranslateLoader,
+				useFactory: createTranslateLoader,
+				deps: [HttpClient],
+			},
+		}),
+	],
+	declarations: [PrescriptionPage],
 })
 export class PrescriptionPageModule {}
